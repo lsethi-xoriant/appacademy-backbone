9488b197383166679cc6c75ccd59ffb311cba399
mpiercy827 <mpiercy@gmail.com>
2015-08-07 11:29:46 -0700

    create users api

---
+++ w7d5/TrelloClone/app/controllers/api/users_controller.rb
@@ -0,0 +1,39 @@
+class Api::UsersController < ApplicationController
+  def index
+    @users = User.all
+    render :json => @users
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      login(@user)
+    else
+      render :json => @user.errors.full_messages, status: :unprocessable_entity
+    end
+  end
+
+  def show
+    @user = User.find(params[:id])
+    render :json => @user
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      render :json => @user
+    else
+      render :json => @user.errors.full_messages, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy!
+    render :json => @user
+  end
+end
