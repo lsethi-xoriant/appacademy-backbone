065772f11c96d28187eb221103259be936ec0636
mpiercy827 <mpiercy@gmail.com>
2015-08-07 12:00:50 -0700

    add new user and session forms

--- w7d5/TrelloClone/app/controllers/api/users_controller.rb
+++ w7d5/TrelloClone/app/controllers/api/users_controller.rb
@@ -8,6 +8,7 @@ class Api::UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       login(@user)
+      fail
     else
       render :json => @user.errors.full_messages, status: :unprocessable_entity
     end
